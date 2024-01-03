3297b67e2edca5a9e65965f3a9effb1982aa6995
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2024-01-03 11:47:26 -0300

    fetch data from the api

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const fetchData = () => {
-  const url = "https://rickandmortyapi.com/api";
+  const url = "https://rickandmortyapi.com/api/character";
 
-  fetch(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((character) => {
+      console.log(character);
+    });
 };
 
 fetchData();
