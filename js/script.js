6cc3ac59706139b6f80dc4b594a330dc6da73820
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2024-01-03 13:00:25 -0300

    fetch info from api and fill the list itens

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 const fetchData = () => {
-  const url = "https://rickandmortyapi.com/api/character";
+  const getCharacterId = (id) =>
+    `https://rickandmortyapi.com/api/character/${id}`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((character) => {
-      console.log(character);
-    });
+  const charactersPromises = [];
+
+  for (let i = 1; i <= 20; i++) {
+    charactersPromises.push(
+      fetch(getCharacterId(i)).then((response) => response.json())
+    );
+  }
+
+  Promise.all(charactersPromises).then((characters) => {
+
+    const lisCharacters = characters.reduce((accumulator, character) => {
+      accumulator += `
+      <li class="card">
+        <img class="card-image" src=${character.image}/>
+          <h2 class="card-title">${character.id} - ${character.name}</h2>
+          <p>${character.status}</p>
+      </li>`
+      return accumulator
+    }, '')
+
+    const ul = document.querySelector('[data-js="rickdex"]')
+
+    console.log(lisCharacters)
+  });
 };
 
 fetchData();
