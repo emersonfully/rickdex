11e59c1965c0c9e7541270c8a434b2a655098f4d
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2024-01-03 14:56:38 -0300

    adjust the color status of each character

--- js/script.js
+++ js/script.js
@@ -4,20 +4,31 @@ const fetchData = () => {
 
   const charactersPromises = [];
 
-  for (let i = 1; i <= 20; i++) {
+  for (let i = 1; i <= 200; i++) {
     charactersPromises.push(
       fetch(getCharacterId(i)).then((response) => response.json())
     );
   }
 
   Promise.all(charactersPromises).then((characters) => {
+    
 
     const lisCharacters = characters.reduce((accumulator, character) => {
+
+      let statusClass 
+      if(character.status === "Alive" ) {
+        statusClass = 'alive'
+      } else if(character.status === 'Dead') {
+        statusClass = 'dead'
+      } else {
+        statusClass = 'unknown'
+      }
+      
       accumulator += `
       <li class="card">
         <img class="card-image" src="${character.image}"/>
           <h2 class="card-title">${character.id} - ${character.name}</h2>
-          <p class="card-subtitle">${character.status}</p>
+          <p class="card-subtitle ${statusClass}">${character.status}</p>
           <p class="card-subtitle">${character.origin.name}</p>
           <p class="card-subtitle">${character.location.name}</p>
           <p class="card-subtitle">${character.species}</p>
